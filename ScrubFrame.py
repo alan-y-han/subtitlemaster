e7dd6e8a0f6ebb722db62966e3fc5debf19794e2
Alan Han <alan.y.han@gmail.com>
2017-01-16 22:00:33 +0000

    Clean up code

--- ScrubFrame.py
+++ ScrubFrame.py
@@ -1,11 +1,15 @@
 import numpy as np
 import cv2
 
+x_start = 254
+x_end = 1030
+y_start = 595
+y_end = 680
+
 
 def erase_subs(img):
-    # img = cv2.imread("/home/alan/cap3.jpg", cv2.IMREAD_COLOR)
     greyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cropped = greyscale[595:680, 254:1030].copy()
+    cropped = greyscale[y_start:y_end, x_start:x_end].copy()
 
     _, thresholded = cv2.threshold(cropped, 200, 255, cv2.THRESH_BINARY)
 
@@ -15,6 +19,7 @@ def erase_subs(img):
     cv2.imshow('dilation', dilated)
 
     cleaned = img.copy()
-    cleaned[595:680, 254:1030] = cv2.inpaint(img[595:680, 254:1030], dilated, 5, cv2.INPAINT_TELEA)
+    cleaned[y_start:y_end, x_start:x_end] = cv2.inpaint(img[y_start:y_end, x_start:x_end], dilated, 5,
+                                                        cv2.INPAINT_TELEA)
 
     return cleaned
--- vidParser.py
+++ vidParser.py
@@ -1,17 +1,14 @@
-import numpy as np
 import cv2
+
 import ScrubFrame
 
 video_in = cv2.VideoCapture('/home/alan/idolmaster/part1.mp4')
-
 video_out = cv2.VideoWriter('/home/alan/idolmaster_test.mp4',
                             cv2.VideoWriter_fourcc(*'X264'),
                             30,
                             (1280, 720))
 
 counter = 0
-
-# no_frames = cap.get(cv2.)
 
 while video_in.isOpened():
     ret, frame = video_in.read()
@@ -20,7 +17,6 @@ while video_in.isOpened():
         counter += 1
         cleanedFrame = ScrubFrame.erase_subs(frame)
 
-        # cv2.imwrite('/media/alan/Transcend/temp/frame' + str(counter) + '.png', cleanedFrame)
         video_out.write(cleanedFrame)
         print('Processed frame ' + str(counter))
     else:
