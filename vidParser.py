2600fa3f3727abb71fd36c312ec092bb299b0068
Alan Han <alan.y.han@gmail.com>
2017-01-16 20:18:23 +0000

    Read video and dump desubtitled frames

--- vidParser.py
+++ vidParser.py
@@ -1,14 +1,30 @@
 import numpy as np
 import cv2
-import scrubFrame
+import ScrubFrame
 
 cap = cv2.VideoCapture('/home/alan/idolmaster/part1.mp4')
 
+# vwriter = cv2.VideoWriter('/home/alan/idolmaster_test.mp4',
+#                           cv2.VideoWriter_fourcc(*'X264'),
+#                           30,
+#                           (1280, 720))
+
+counter = 0
+
 while cap.isOpened():
+    counter += 1
+
     _, frame = cap.read()
 
-    cv2.imshow('frame', frame)
-    cv2.waitKey(0)
+    cleanedFrame = ScrubFrame.erase_subs(frame)
+
+    # cv2.imshow('ori frame', frame)
+    # cv2.imshow('clean frame', cleanedFrame)
+    # cv2.waitKey(0)
+    cv2.imwrite('/media/alan/Transcend/temp/frame' + str(counter) + '.png', cleanedFrame)
+
+    # if counter > 10:
+    #     break
 
 cap.release()
 cv2.destroyAllWindows()
